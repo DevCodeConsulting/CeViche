fe31293ed409d45e9ee83be23a6b9c555a67c870
Martin Engelsøy Norum <martin.norum@gmail.com>
2017-02-04 10:34:03 +0100

    Add Drawer

--- src/components/LayoutComponent/LayoutComponent.js
+++ src/components/LayoutComponent/LayoutComponent.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 export default (props) => (
   <div>
     <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" />
+    <Drawer open={false}>
+      <MenuItem>Resume</MenuItem>
+      <MenuItem>Consultants</MenuItem>
+      <MenuItem>Consultants Details</MenuItem>
+    </Drawer>
     {props.children}
   </div>
 )
