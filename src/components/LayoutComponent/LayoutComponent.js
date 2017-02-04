934c79e3baacc80b377a54e0270186a9c4fb0109
Martin Engelsøy Norum <martin.norum@gmail.com>
2017-02-04 10:49:56 +0100

    Open drawer

--- src/components/LayoutComponent/LayoutComponent.js
+++ src/components/LayoutComponent/LayoutComponent.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
-export default (props) => (
-  <div>
-    <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" />
-    <Drawer open={false}>
-      <MenuItem>Resume</MenuItem>
-      <MenuItem>Consultants</MenuItem>
-      <MenuItem>Consultants Details</MenuItem>
-    </Drawer>
-    {props.children}
-  </div>
-)
+export default class LayoutComponent extends Component {
+  constructor () {
+    super();
+    this.state = { drawerIsOpen: false }
+    this.handleHamburgerTouch = this.handleHamburgerTouch.bind(this);
+  }
+
+  handleHamburgerTouch() {
+    this.setState({drawerIsOpen: !this.state.drawerIsOpen});
+  }
+
+  render () {
+    return (
+      <div>
+        <AppBar title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={this.handleHamburgerTouch}
+          />
+        <Drawer open={this.state.drawerIsOpen}>
+          <MenuItem>Resume</MenuItem>
+          <MenuItem>Consultants</MenuItem>
+          <MenuItem>Consultants Details</MenuItem>
+        </Drawer>
+        {this.props.children}
+      </div>
+    )
+  }
+}
