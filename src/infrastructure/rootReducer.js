6b4a0c7763bcf776d4f0924e1d2860ac71c50e95
Martin Blodau <mblodau@me.com>
2017-02-04 11:08:52 +0100

    Working on the consultants actions and reducer.

--- src/infrastructure/rootReducer.js
+++ src/infrastructure/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers }                                      from 'redux';
 import { routerReducer }                                        from 'react-router-redux';
 import exampleReducer                                           from '../reducers/exampleReducer';
+import consultantsReducer                                       from '../reducers/consultantsReducer';
 
 
 export default combineReducers({
     exampleReducer,
-
+    consultantsReducer,
     routing: routerReducer
 });
