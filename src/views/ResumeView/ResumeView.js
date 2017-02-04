585d4e0a030c277ffe07dfa3ced99b66d1fce81f
ssolders <simon.solders@devcode.se>
2017-02-04 11:07:04 +0100

    First reducer + action to set mock data in a resume

--- src/views/ResumeView/ResumeView.js
+++ src/views/ResumeView/ResumeView.js
@@ -1,27 +1,64 @@
 import React                                                    from "react";
+import { connect }                                              from "react-redux";
 import shouldPureComponentUpdate                                from "react-pure-render/function";
 
 import LayoutComponent                                          from "~/components/LayoutComponent/LayoutComponent"
+import * as ResumeActions                                       from "~/actions/resumeActions";
 
 import "./ResumeView.scss";
 
-
+function mapStateToProperties(store) {
+    return {
+        resumeData: store.resumeReducer
+    };
+}
 
 class ResumeView extends React.Component {
     constructor(...props) {
         super(...props);
         this.shouldComponentUpdate = shouldPureComponentUpdate.bind(this);
+
+        this._renderCompetences = this._renderCompetences.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.setResumeData();
+    }
+
+    _renderCompetences() {
+        const competence = this.props.resumeData.competence
+
+        return competence.map((comp, i) => {
+            console.log(comp.name);
+            console.log(comp.skillevel);
+            console.log(comp.category);
+            return (
+                <div key={i}>
+                    <h3>{comp.name}</h3>
+                    <h3>{comp.skillevel}</h3>
+                    <h3>{comp.category}</h3>
+                </div>
+            );
+        })
     }
 
     render () {
+        const { resumeData } = this.props,
+            { competence } = resumeData;
+
         return (
-            <LayoutComponent>
+            <LayoutComponent title={"Resume"}>
                 <div className="resume-view-container">
-                    <h1>Resume view</h1>
+
+                    <h1>Competence</h1>
+                    { this._renderCompetences() }
+
                 </div>
             </LayoutComponent>
         );
     }
 }
 
-export default ResumeView;
+
+
+export default connect(mapStateToProperties, ResumeActions)(ResumeView);
