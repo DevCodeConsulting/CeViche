250511991d1c18fcc48b176c50157a79fd1bdbdb
ssolders <simon.solders@devcode.se>
2017-02-04 08:42:11 +0100

    Added routes + empty pages for each route

--- src/infrastructure/routes.js
+++ src/infrastructure/routes.js
@@ -5,13 +5,20 @@ import Router                                                   from "react-rout
 import Redirect                                                 from "react-router/lib/Redirect";
 
 //Views + Headers
-import HomeView                                              from "~/views/HomeView/HomeView";
+import HomeView                                                 from "~/views/HomeView/HomeView";
+import ConsultantsListView                                      from "~/views/ConsultantsListView/ConsultantsListView";
+import ConsultantDetailView                                     from "~/views/ConsultantDetailView/ConsultantDetailView";
+import CvView                                                   from "~/views/CvView/CvView";
 
 export default (history, store) => {
 
     return (
         <Router history={history}>
             <Route name="Start Page" staticName={true} path="/"  component={HomeView} />
+
+            <Route name="Consultants" staticName={true} path="/consultants"  component={ConsultantsListView} />
+            <Route name="Consultant Details" staticName={true} path="/consultant-details/:consultant"  component={ConsultantDetailView} />
+            <Route name="CV" staticName={true} path="/cv/:consultant"  component={CvView} />
         </Router>
     );
 };
